80da51dce1e442b6e1931461df245b1db56e98fa
Gabriel Badarau <badaraugabriel95@gmail.com>
2024-09-08 20:07:46 +0300

    Add AnimeList Client

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,25 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import { MovieDb } from 'moviedb-promise';
+import { MALClient } from "@animelist/client";
+
+
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 const moviedb = new MovieDb(process.env.TMDB_API_KEY as string)
+const mal = new MALClient({clientId:process.env.MAL_CLIENT_ID as string});
 
 app.get('/', async (req:Request, res:Response) => {
   res.send('Express + TypeScript Server');
 
   const movie = await moviedb.searchMovie({ query: 'alien' })
-  console.log(movie)
+
+
+  // const result = await mal.getAnimeList({q:'naruto'})
+  // console.log(result.data[0])
 });
 
 app.listen(port, () => {
