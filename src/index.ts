82248102aa9c9874852a4dbb4ae88c6e2e3493c3
Gabriel Badarau <badaraugabriel95@gmail.com>
2024-09-08 18:58:47 +0300

    Add TMDB Node Wrapper

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import { MovieDb } from 'moviedb-promise';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
+const moviedb = new MovieDb(process.env.TMDB_API_KEY as string)
 
-app.get('/', (req:Request, res:Response) => {
+app.get('/', async (req:Request, res:Response) => {
   res.send('Express + TypeScript Server');
+
+  const movie = await moviedb.searchMovie({ query: 'alien' })
+  console.log(movie)
 });
 
 app.listen(port, () => {
