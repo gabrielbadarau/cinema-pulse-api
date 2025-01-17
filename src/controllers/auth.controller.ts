fc2885f5140b2f9ae71e2ddc1ece0a02b6df653f
Gabriel Badarau <badaraugabriel95@gmail.com>
2025-01-17 14:32:22 +0200

    Change to HTTP cookie for refresh token

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -71,7 +71,15 @@ export const login = async (req: Request, res: Response) => {
     const accessToken = generateAccessToken(user.id);
     const refreshToken = await generateRefreshToken(user.id);
 
-    res.json({ accessToken, refreshToken });
+    res.cookie('refreshToken_cinema_pulse_api', refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+      path: '/',
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    res.json({ accessToken });
   } catch (error) {
     console.error('Login error:', error);
     res.status(500).json({ error: 'Internal server error' });
--- src/middlewares/cors.middleware.ts
+++ src/middlewares/cors.middleware.ts
@@ -1,4 +1,5 @@
 import { Application } from 'express';
 import cors from 'cors';
 
-export const applyCorsMiddleware = (app: Application) => app.use(cors());
+export const applyCorsMiddleware = (app: Application) =>
+  app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
